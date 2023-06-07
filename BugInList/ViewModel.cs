3030028a69f4e637fbfb380c248e49fdea920f9b
Alexey Tkachenko <tkachenko@peleng.by>
2023-06-07 16:44:22 +0300

    Demo project

---
+++ BugInList/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BugInList
+{
+    public class ViewModel
+    {
+        public List<ViewModelItem> Items { get; } = new()
+        {
+            new ViewModelItem("First"),
+            new ViewModelItem("Second"),
+            new ViewModelItem("Third"),
+        };
+    }
+
+    public class ViewModelItem : INotifyPropertyChanged
+    {
+        public ViewModelItem(string initialValue)
+        {
+            this.value = initialValue;
+        }
+
+        private string value;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Value
+        {
+            get => value;
+            set
+            {
+                if (this.value != value)
+                {
+                    this.value = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                }
+            }
+        }
+
+    }
+}
